8b44cb3633070088898dabbd5e82fc9babdf6f56
CayoLP <caypcayolibanio@gmail.com>
2025-05-21 20:56:11 -0300

    primeiro commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numeros = new int[5];
+        for (int i = 0; i <= 5; i++) {
+            numeros[i] = i * 2;
+        }
+
+        String nome = null;
+        if (nome.equals("teste")) {
+            System.out.println("Nome é teste");
+        }
+
+        int x = 10;
+        if (x = 5) {
+            System.out.println("X é 5");
+        }
+
+        int y = 0;
+        int resultado = 10 / y;
+
+        System.out.println("Programa terminou");
+    }
+}
